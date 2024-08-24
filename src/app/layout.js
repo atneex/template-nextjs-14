d62980a32f5d4c7c26b6c8c389be7a095cef6850
unknown <atneex@gmail.com>
2024-08-24 17:27:17 +0200

    adapt html and body width

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,14 @@
 import '@/styles/global.scss';
+import style from './layout.module.scss';
 
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-      <body>{children}</body>
+      <body id={style.root}>
+        <main>
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
